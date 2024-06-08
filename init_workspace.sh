2e28c48252558e3df5c6d839732928207c2751ab
HC-85 <hugoce.85@gmail.com>
2024-06-07 23:44:04 -0600

    no conflict?

--- init_workspace.sh
+++ init_workspace.sh
@@ -14,6 +14,12 @@ fi
 
 sudo mkdir -p /workspaces/Nutrition-Logger/audio/voicenotes
 
+scipy_version=$(python3 -c "import scipy; print(scipy.__version__)")
+
+if [ "$scipy_version" == "1.13.1" ]; then
+sudo tailscale up --accept-routes --authkey $TAILSCALE_KEY
+sudo mkdir -p /workspaces/Nutrition-Logger/audio/voicenotes
+
 sudo mkdir -p /mnt/local
 sudo chown codespace:codespace /mnt/local
 
