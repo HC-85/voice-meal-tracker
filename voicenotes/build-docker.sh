40e40f493fdf28b1a3334b709481794b257e7d62
ZR-85 <zrktr.85.0@gmail.com>
2024-08-19 00:57:05 +0000

    Removed bash scripts

--- voicenotes/build-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-docker build \
-    --build-arg _TWILIO_ACCOUNT_SID=${TWILIO_ACCOUNT_SID} \
-    --build-arg _TWILIO_AUTH_TOKEN=${TWILIO_AUTH_TOKEN} \
-    -t voicenote-fetching .
--- voicenotes/run-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-docker run \
-    -p 8000:8000 \
-    -v /workspaces/voice-meal-tracker/voicenotes/vn_cache:/app/vn_cache \
-    voicenote-fetching
