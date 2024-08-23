4bcee3f993fedf8ee69b9e87e32cc6f6a625ab84
ZR-85 <zrktr.85.0@gmail.com>
2024-08-23 01:44:17 +0000

    transcription update

--- transcription_service/api_transcribe.py
+++ transcription_service/api_transcribe.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
-from app_transcribe import load_faster_whisper
+from pydantic import BaseModel
+from app_transcribe import load_faster_whisper, transcribe
 
 app = FastAPI()
+
+class Audio(BaseModel):
+    file_path: str
 
 @app.on_event("startup")
 def load_model():
@@ -10,5 +14,10 @@ def load_model():
 
 
 @app.post("/transcribe")
-def transcribe_endpoint():
-    pass
+def transcribe_endpoint(audio: Audio):
+    transcription = transcribe(model, audio.file_path)
+    return transcription
+
+@app.get("/health")
+def healthcheck():
+    return "OK"
