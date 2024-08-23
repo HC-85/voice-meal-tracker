c87253bf2e30a9763091688b949a85058e0771b1
ZR-85 <zrktr.85.0@gmail.com>
2024-08-23 23:40:41 +0000

    Transcriptions are now written to file

--- transcription_service/api_transcribe.py
+++ transcription_service/api_transcribe.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from app_transcribe import load_faster_whisper, transcribe
+from app_transcribe import load_faster_whisper, transcribe_all, transcribe_audio
 
 app = FastAPI()
 
@@ -14,10 +14,17 @@ def load_model():
     model = load_faster_whisper(model_size='distil-small.en')
 
 
-@app.post("/transcribe")
+@app.post("/transcribe_audio")
 def transcribe_endpoint(audio: Audio):
-    transcription = transcribe(model, audio.file_path)
+    transcription = transcribe_audio(model, audio.file_path)
     return {"transcription": transcription}
+
+
+@app.post("/transcribe_all")
+def transcribe_all_endpoint():
+    transcription = transcribe_all(model)
+    return {"transcription": transcription}
+
 
 @app.get("/health")
 def healthcheck():
