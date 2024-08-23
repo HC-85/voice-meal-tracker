979f6530763464cc53b10db92f5bd99a82e0d95f
ZR-85 <zrktr.85.0@gmail.com>
2024-08-23 08:04:33 +0000

    Transcription API now works

--- transcription_service/api_transcribe.py
+++ transcription_service/api_transcribe.py
@@ -7,16 +7,17 @@ app = FastAPI()
 class Audio(BaseModel):
     file_path: str
 
+
 @app.on_event("startup")
 def load_model():
     global model
-    model = load_faster_whisper()
+    model = load_faster_whisper(model_size='distil-small.en')
 
 
 @app.post("/transcribe")
 def transcribe_endpoint(audio: Audio):
     transcription = transcribe(model, audio.file_path)
-    return transcription
+    return {"transcription": transcription}
 
 @app.get("/health")
 def healthcheck():
--- transcription_service/app_transcribe.py
+++ transcription_service/app_transcribe.py
@@ -1,19 +1,11 @@
-from typing import Union, Dict
-
-MODEL_ID = "openai/whisper-tiny"
-MODEL_SAMPLE_RATE = 16_000
-
 from faster_whisper import WhisperModel
 
-def load_faster_whisper(model_size = "large-v3"):
+
+def load_faster_whisper(model_size = "tiny.en"):
     model = WhisperModel(model_size, device="cpu", compute_type="int8")
     return model
 
 
 def transcribe(model, audio_file):
-    
-    segments, info = model.transcribe(audio_file, beam_size=5)
-
-    print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
-
-    return list(segments)
+    segments, _ = model.transcribe(audio_file, beam_size=5)
+    return [segment.text for segment in segments]
